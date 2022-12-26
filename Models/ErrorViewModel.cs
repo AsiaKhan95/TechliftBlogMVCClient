576c25b056283f46c7e82d3ee551f47754bfd6fc
Asia Khan <asia2603@yahoo.com>
2022-12-26 10:20:59 +0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TechliftBlogMVCClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
